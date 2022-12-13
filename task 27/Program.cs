f4d6afe03dfdc270a5a422418e4fbefcc5042e09
uslan <bublikrs5@mail.ru>
2022-12-13 15:55:59 +0300

    first commit

---
+++ task 27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int total = 0;
+while(num != 0)
+{
+    total += num%10;
+    num = num/10;
+}
+Console.WriteLine("Сумма цифр числа равна: " + total);
